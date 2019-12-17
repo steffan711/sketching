6688b386b44eff878272a1c2c4ce8f391ea4d10c
Sebastian Junges <sebastian.junges@rwth-aachen.de>
2019-12-17 15:35:40 +0100

    further tests

--- dynasty_tests/test_regression.py
+++ dynasty_tests/test_regression.py
@@ -8,12 +8,20 @@ import dynasty.cli
 logger = logging.getLogger(__name__)
 
 benchmarks_feasibility = [
-    pytest.param("examples/grid", "4x4grid_sl.templ", "CMAX=11,T_EXP=10.0,T_SLOW=10.0,T_FAST=0.7", "4x4grid_sl.allowed", "single.properties",  "none.restrictions", "lift"),
-   # pytest.param("grid", "4x4grid_sl.templ", "", "property1", "prism"),
-   # pytest.param("brp", "brp", "N=16,MAX=2", "property1", "stormpy", marks=[require_stormpy()]),
+    pytest.param("examples/grid", "4x4grid_sl.templ", "CMAX=11,T_EXP=10.0,T_SLOW=10.0,T_FAST=0.7", "4x4grid_sl.allowed", "single.properties",  "none.restrictions"),
+    pytest.param("examples/grid", "4x4grid_sl.templ", "CMAX=11,T_EXP=10.0,T_SLOW=10.0,T_FAST=0.9", "4x4grid_sl.allowed",
+                 "single.properties", "none.restrictions"),
+    ## pytest.param("brp", "brp", "N=16,MAX=2", "property1", "stormpy", marks=[require_stormpy()]),
 ]
 
-@pytest.mark.parametrize("project,sketch,constants,allowed,properties,restrictions,method", benchmarks_feasibility)
+methods_feasibility = [
+    "lift",
+    "cschedenum",
+    "cegis"
+]
+
+@pytest.mark.parametrize("project,sketch,constants,allowed,properties,restrictions", benchmarks_feasibility)
+@pytest.mark.parametrize("method", methods_feasibility)
 def test_feasibility_script(project, sketch, constants, allowed, properties, restrictions, method):
     command = ["--project",
                project,
